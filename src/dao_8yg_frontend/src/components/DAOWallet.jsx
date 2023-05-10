f8df7cf42c54a3c1ee183b71ea4ce117a7c7eb79
James Beadle <jamesbeadle1989@gmail.com>
2023-05-10 23:36:08 +0100

    Loaded images

--- src/dao_8yg_frontend/src/components/DAOWallet.jsx
+++ src/dao_8yg_frontend/src/components/DAOWallet.jsx
@@ -4,11 +4,13 @@ import { LinkContainer } from 'react-router-bootstrap';
 import WalletImage from '../../assets/dao_wallet.jpg';
 import { StoicIdentity } from "ic-stoic-identity";
 import { dao_8yg_backend as backend } from '../../../declarations/dao_8yg_backend';
+import CustomSVG from './CustomSVG';
 
 
 const DAOWallet = () => {
   
   const [isLoading, setIsLoading] = useState(true);
+  const [nfts, setNfts] = useState([]);
   
   useEffect(() => {
 
@@ -16,10 +18,26 @@ const DAOWallet = () => {
 
     const fetchData = async () => {
       var nftWallet = await backend.getNFTWallet();
+
+      /*var nftWallet = [
+        {
+            "tokenId": "hls6x-4ikor-uwiaa-aaaaa-buaf4-uaqca-aaant-a",
+            "tokenIndex": 870,
+            "canisterId": "tskpj-aiaaa-aaaag-qaxsq-cai",
+            "accountIdentifier": "0fa2901a7d5b36b1412ae14fc8c71ae424a7977930f59d230a0eb494f5e1b3c6"
+        },
+        {
+            "tokenId": "djtqm-hakor-uwiaa-aaaaa-deaha-qaqca-aaaae-q",
+            "tokenIndex": 9,
+            "canisterId": "buja2-4iaaa-aaaam-qa4ca-cai",
+            "accountIdentifier": "0fa2901a7d5b36b1412ae14fc8c71ae424a7977930f59d230a0eb494f5e1b3c6"
+        }
+    ];
+    */
+
+
+      setNfts(nftWallet);
       console.log(nftWallet);
-      
-      var cycles = await backend.getCycles();
-      console.log(Number(cycles));
       
       setIsLoading(false);
     };
@@ -60,15 +78,24 @@ const DAOWallet = () => {
     (
       <Container className="flex-grow-1 my-5">
         <Row>
-          <Col md={6}>
-            <img src={WalletImage} alt="8yg" className='mt-2 mb-4' style={{ maxWidth: '100%', maxHeight: '100%', marginRight: '10px' }} />
-          </Col>
-          <Col md={6}>
+          <Col md={12}>
             <Row>
-              <Col md={12}>
-                <h1>The DAO Wallet</h1>
-                <p className='mt-4 mb-4'>A list of the DAO's NFTs will appear here soon.</p>
+              {nfts.map((nft) => (
+                <Col md={4} key={nft.tokenIndex}>
+                  <Card className="mb-4">
+                    
+                  <CustomSVG
+                      canisterId={nft.canisterId}
+                      tokenId={nft.tokenId}
+                      className="custom-svg"
+                      style={{ width: "100%" }}
+                    />
+                  <Card.Body>
+                    <Card.Title>NFT #{nft.tokenIndex + 1}</Card.Title>
+                  </Card.Body>
+                  </Card>
                 </Col>
+              ))}
             </Row>
           </Col>
         </Row>
@@ -80,7 +107,8 @@ const DAOWallet = () => {
 export default DAOWallet;
 
 /*
-
+<Card.Img variant="top" src={`https://${nft.canisterId}.raw.ic0.app/?type=thumbnail&?tokenid=${nft.tokenId}`} />
+                
 
                 <p className='mt-4 mb-4'>Filter by collection.</p>
                 <p className='mt-4 mb-4'>Filter by for sale or not.</p>
